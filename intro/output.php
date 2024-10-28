c8c9f4e439fb0b3edbe9c49e184030fd1ec88c32
mohammed el aouri <mohammedelaouri@gmail.com>
2024-10-28 17:34:40 +0100

    My full course about php and xampp web server

---
+++ intro/output.php
@@ -0,0 +1,20 @@
+<?php
+/**
+With PHP, there are two basic ways to get output: echo and print.
+In this tutorial we use echo or print in almost every example.
+So, echo and print are more or less the same. They are both used to output data to the screen.
+
+The differences are small:
+ * echo has no return value while print has a return value of 1, so it can be used in expressions.
+ * echo can take multiple parameters (although such usage is rare) while print can take one argument.
+ * echo is marginally faster than print.
+ */
+
+echo "Hello World !";
+print "Hello World !";
+$userName="userName";
+$userAge="userAge";
+print "<h1>'Hello'.$userName.'your Age'.$userAge</h1>";
+
+
+
